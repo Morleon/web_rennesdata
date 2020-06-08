d3e4d8e966b162210acc4dc3b18c66c8cbefb4f8
mpoire <poire.e1700917@etud.univ-ubs.fr>
2020-06-08 17:40:23 +0200

    gestion des conflits

--- src/app/showdata/showdata.component.ts
+++ src/app/showdata/showdata.component.ts
@@ -22,15 +22,199 @@ export class ShowdataComponent implements OnInit {
 		 {value: '9', viewValue: '9ème Circonscription'},
 		 {value: '10', viewValue: '10ème Circonscription'},
 	];
-	perc_part = [donnees.records[0].fields.pourcentage_participation, donnees.records[1].fields.pourcentage_participation]	
 
+	// permet d'effectuer l'import
 	constructor(private apiService: ApiService) { }
 
-	ngOnInit(): void {
+	ngOnInit() {
 		this.apiService.getInfo().subscribe((data)=>{
 			this.donnees = data;
-			console.log(this.donnees);
-			console.log(this.donnees.records[1].fields);
+			console.log(this.donnees)
+			this.createLists(0)
 		});
 	}
+
+	createLists(num_circ) {
+
+		if (num_circ == null){
+			num_circ = 0
+		}
+
+		this.resultats = new Array();
+		this.candidats = new Array();
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_1;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_1;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_2;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_2;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_3;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_3;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_4;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_4;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_5;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_5;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_6;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_6;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_7;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_7;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_8;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_8;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_9;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_9;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_10;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_10;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_11;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_11;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_12;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_12;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_13;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_13;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_14;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_14;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_15;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_15;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_16;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_16;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_17;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_17;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_18;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_18;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_19;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_19;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_20;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_20;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_21;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_21;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_22;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_22;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_23;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_23;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_24;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_24;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_25;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_25;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_26;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_26;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_27;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_27;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_28;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_28;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_29;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_29;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_30;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_30;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_31;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_31;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_32;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_32;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_33;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_33;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		var candidat = this.donnees.records[num_circ].fields.candidat_34;
+		var resultat = this.donnees.records[num_circ].fields.pourcentage_34;
+		this.candidats.push(candidat);
+		this.resultats.push(resultat);
+
+		console.log(this.resultats)
+		console.log(this.candidats)
+	}
+
 }
