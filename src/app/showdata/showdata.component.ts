92e8b76a7b634095cf0382263f5e08922ffc725a
MLebastard <lebastard.maxime7@gmail.com>
2020-06-08 22:15:36 +0200

    showdata.component.html

--- src/app/showdata/showdata.component.ts
+++ src/app/showdata/showdata.component.ts
@@ -11,18 +11,18 @@ export class ShowdataComponent implements OnInit {
 	objectKeys = Object.keys;
 
 	donnees;
-	circs = [{value: '1', viewValue: '1ère Circonscription'},
-		 {value: '2', viewValue: '2ème Circonscription'},
-		 {value: '3', viewValue: '3ème Circonscription'},
-		 {value: '4', viewValue: '4ème Circonscription'},
-		 {value: '5', viewValue: '5ème Circonscription'},
-		 {value: '6', viewValue: '6ème Circonscription'},
-		 {value: '7', viewValue: '7ème Circonscription'},
-		 {value: '8', viewValue: '8ème Circonscription'},
-		 {value: '9', viewValue: '9ème Circonscription'},
-		 {value: '10', viewValue: '10ème Circonscription'},
+	circs = [{value: '1', viewValue: "Cité Internationale Paul Ricoeur"},
+		 {value: '2', viewValue: "Groupe Scolaire Marcel Pagnol"},
+		 {value: '3', viewValue: "Groupe Scolaire des Clôteaux"},
+		 {value: '4', viewValue: "Groupe Scolaire Jules Isaac"},
+		 {value: '5', viewValue: "Groupe Scolaire Carle Bahon"},
+		 {value: '6', viewValue: "Groupe Scolaire des Clôteaux"},
+		 {value: '7', viewValue: "Gymnase Lesseps"},
+		 {value: '8', viewValue: "Groupe Scolaire de l'Ille"},
+		 {value: '9', viewValue: ''},
+		 {value: '10', viewValue: "Groupe Scolaire Jean Rostand"},
 	];
-	perc_part = [donnees.records[0].fields.pourcentage_participation, donnees.records[1].fields.pourcentage_participation]	
+/*	perc_part = [this.donnees.records[0].fields.pourcentage_participation, this.donnees.records[1].fields.pourcentage_participation];*/
 
 	constructor(private apiService: ApiService) { }
 
